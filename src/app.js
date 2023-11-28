f05065e82dec143d268d1ed547f73ea5979eb6d1
Karen Lyon <144794853+Karelyon@users.noreply.github.com>
2023-11-28 13:40:49 +0000

    finsh

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateexcuse();
+};
+
+let generateexcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomwho = Math.floor(Math.random() * who.length);
+  let randomaction = Math.floor(Math.random() * action.length);
+  let randomwhat = Math.floor(Math.random() * what.length);
+  let randomwhen = Math.floor(Math.random() * when.length);
+
+  return (
+    who[randomwho] +
+    " " +
+    action[randomaction] +
+    " " +
+    what[randomwhat] +
+    " " +
+    when[randomwhen]
+  );
 };
